bbacce14bbb463cfbed998361561dab7d526d91c
Grant Kluber <gkluber@sbcglobal.net>
2019-11-19 16:59:40 -0600

    Setting up project structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='subriemannian-qc',
+      version='1.0',
+      author='Grant Kluber',
+      url='gkluber.github.io',
+      packages=['subriemannian_qc'])
