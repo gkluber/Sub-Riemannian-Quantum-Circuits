d14552b2e51c58521a3a35d457348c4de4734fcb
Grant Kluber <gkluber@sbcglobal.net>
2019-11-23 12:08:15 -0600

    Worked on generating the Lie group/algebra for SU(2^n)

--- subriemannian_qc/util.py
+++ subriemannian_qc/util.py
@@ -1,10 +1,38 @@
 from typing import List
 import numpy as np
 
+X = np.array([0, 1], [1, 0], dtype=np.complex64)
+Y = np.array([0, 1j], [-1j, 0], dtype=np.complex64)
+Z = np.array([1, 0], [0, -1], dtype=np.complex64)
+
 
 def generate_lie_group(n: int):
-    basis = [1, 2, 3, 4]
-    np.meshgrid(basis, basis, basis, basis).T.reshape(-1, )
+    basis_indices = [0, 1, 2, 3]
+
+    # Need to exclude the [0, 0, 0, 0] identity matrix
+    basis_combs = combinations(basis_indices, n)[1:]
+
+
+def get_pauli_tensor(index_string: List[int]) -> np.ndarray:
+    size = len(index_string)
+    if size == 0:
+        return np.identity(2)
+
+    result = get_pauli(index_string[0])
+    for x in range(1, size):
+        result = np.kron(result, get_pauli(index_string[x]))
+    return result
+
+
+def get_pauli(index: int) -> np.ndarray:
+    if index == 0:
+        return np.identity(2)
+    elif index == 1:
+        return X
+    elif index == 2:
+        return Y
+    elif index == 3:
+        return Z
 
 
 def generate_lie_algebra(n: int):
